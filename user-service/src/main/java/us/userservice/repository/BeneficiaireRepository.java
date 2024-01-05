a9dd309d504da2da5174e2b3433fecd421df400a
HanaeElOmranii <hanaeelomrani12@gmail.com>
2024-01-05 12:50:46 +0100

    add Beneficiaire methodes

--- user-service/src/main/java/us/userservice/repository/BeneficiaireRepository.java
+++ user-service/src/main/java/us/userservice/repository/BeneficiaireRepository.java
@@ -11,8 +11,10 @@ import java.util.List;
 @Repository
 public interface BeneficiaireRepository extends JpaRepository<Beneficiaire,Long> {
     List<Beneficiaire> findAllByClient_Id(Long clientId);
-    @Query("SELECT b FROM Beneficiaire b WHERE (b.nom LIKE %:term%) OR (b.prenom LIKE %:term%)")
+    @Query("SELECT b FROM Beneficiaire b WHERE (b.nom LIKE %:term%) OR (b.prenom LIKE %:term%) OR (b.walletCode LIKE %:term%) ")
     List<Beneficiaire> searchBeneficiaireByTerm(@Param("term") String term);
+
+
 
     @Query("SELECT b.isBlockListed FROM Beneficiaire b WHERE b.id = :id")
     Boolean isBeneficiaireBlockListed(@Param("id") Long id);
