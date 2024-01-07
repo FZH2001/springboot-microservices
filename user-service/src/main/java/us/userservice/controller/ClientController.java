3e034979254392aa86904907c421959c9eed97df
IsmailKaou <ismailkaou12@gmail.com>
2024-01-07 02:12:24 +0100

    modified on apigateway config CORS

--- user-service/src/main/java/us/userservice/controller/ClientController.java
+++ user-service/src/main/java/us/userservice/controller/ClientController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/client")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ClientController {
     private final ClientService clientService;
 
